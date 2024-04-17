45adceb04f131982a489fba3876ce47cc0448724
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-17 20:49:46 +0300

    Added changes for deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -114,9 +114,6 @@ DJOSER = {
     'SERIALIZERS': {'user_create': 'apis.serializers.CustomUserCreateSerializer'}
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Moscow'
